f867770ad01d5d0e229ea557ce0af1395f8cd7ee
Youssef <youssef.osm2000@gmail.com>
2025-01-24 16:22:53 +0100

    Courseinfo updated

--- trials/src/App.jsx
+++ trials/src/App.jsx
@@ -1,17 +1,22 @@
-const App = () => {
-  const arr = [1, -3, 15]
-  console.log(arr)
-  arr.push(-12)
-  
-  
-  arr.forEach((value) => {
-    console.log(value)
-  })
+const sum = (p1, p2) => {
+  console.log(p1)
+  console.log(p2)
+  return p1 + p2
+}
 
-  
+const square = p => p * p
+
+function foo(){
+  return 'bar'
+}
+
+const App = () => {
+  const t = [1, 2, 3, 4, 5, 6]
+  const t2 = t.map(square)
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{t}</h1>
+      <h1>{foo()}</h1>
     </>
   )
 }
